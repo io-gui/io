dd6549daa66aae2c105cbeb102a6017acb6b86c3
Aki Rodic <aleksandar.xyz@gmail.com>
2018-06-02 12:55:41 -0400

    Added listeners to io-node.

--- src/io-node.js
+++ src/io-node.js
@@ -6,6 +6,7 @@ export class IoNode {
   constructor() {
     this.__proto__.constructor.Register();
     Object.defineProperty(this, '__props', { value: this.__proto__._properties.clone() } );
+    Object.defineProperty(this, '__listeners', { value: {} } );
     for (let prop in this.__props) {
       this.defineProperty(prop);
       this[prop] = this[prop];
@@ -34,6 +35,32 @@ export class IoNode {
     // TODO
   }
   update(dtime, time, progress) {}
+  addEventListener( type, listener ) {
+		this.__listeners[ type ] = this.__listeners[ type ] || [];
+		if ( this.__listeners[ type ].indexOf( listener ) === - 1 ) {
+			this.__listeners[ type ].push( listener );
+		}
+	}
+	hasEventListener( type, listener ) {
+		return this.__listeners[ type ] !== undefined && this.__listeners[ type ].indexOf( listener ) !== - 1;
+	}
+	removeEventListener( type, listener ) {
+		if ( this.__listeners[ type ] !== undefined ) {
+			var index = this.__listeners[ type ].indexOf( listener );
+			if ( index !== - 1 ) {
+				this.__listeners[ type ].splice( index, 1 );
+			}
+		}
+	}
+	dispatchEvent( type, detail ) {
+    var event = { detail: detail };
+		if ( this.__listeners[ type ] !== undefined ) {
+			var array = this.__listeners[ type ].slice( 0 );
+			for ( var i = 0, l = array.length; i < l; i ++ ) {
+				array[ i ].call( this, { detail: detail, target: this } );
+			}
+		}
+	}
 }
 
 IoNode.Register = function() {
