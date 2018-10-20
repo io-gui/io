48eaac5aaaa5c26bc013d080ab9531abd9e9dba9
Aki Rodic <aleksandar.xyz@gmail.com>
2018-10-20 10:02:38 +0200

    npm update.

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,6 @@
-import path from 'path';
-
-function html() {
-  return {
-    transform( code, id ) {
-      var transformedCode = code;
-      var regex = /html`(([\s\S])*)`/;
-      if ( regex.test( code ) === true ) {
-        let match = code.match(regex);
-        transformedCode = code.replace(match[0], match[0].replace((/  |\r\n|\n|\r/gm),""))
-      };
-      return {
-        code: transformedCode,
-        map: { mappings: '' }
-      };
-    }
-  };
-}
-
 export default [
   {
     input: 'src/io.js',
-    experimentalDynamicImport: true,
-    plugins: [
-      html()
-    ],
     output: [
       {
         format: 'es',
