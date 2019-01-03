91ab0b38dd37c53b37861379ec76c3034b49889b
Aki Rodic <aleksandar.xyz@gmail.com>
2019-01-03 12:44:42 -0500

    Removed auto-binding from getter and setter functions.

--- src/core/util/protoFunctions.js
+++ src/core/util/protoFunctions.js
@@ -1,4 +1,5 @@
-// Creates a list of functions defined in prototype chain.
+// Creates a list of functions defined in prototype chain (for the purpose of binding to instance).
+// TODO: consider improving
 export class ProtoFunctions extends Array {
   constructor(prototypes) {
     super();
@@ -6,10 +7,10 @@ export class ProtoFunctions extends Array {
       let names = Object.getOwnPropertyNames(prototypes[i]);
       for (let j = 0; j < names.length; j++) {
         if (names[j] === 'constructor') continue;
+        const p = Object.getOwnPropertyDescriptor(prototypes[i], names[j]);
+        if (p.get || p.set) continue;
         if (typeof prototypes[i][names[j]] !== 'function') continue;
-        if (prototypes[i][names[j]].name === 'anonymous') {
-          continue;
-        }
+        if (prototypes[i][names[j]].name === 'anonymous') continue;
         if (this.indexOf(names[j]) === -1) this.push(names[j]);
         if (names[j] === 'value') console.log(prototypes[i][names[j]]);
       }
