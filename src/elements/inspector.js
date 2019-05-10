abc249c4b88b13061254ea4f9a119eb606e244d7
AKI RODIC <aleksandar.xyz@gmail.com>
2019-05-10 16:43:34 +0200

    Moved inspector link and breadcrumbs to inspector.js.

--- src/elements/inspector.js
+++ src/elements/inspector.js
@@ -1,8 +1,6 @@
 import {IoElement, html} from "../core/element.js";
 import {IoStorage} from "../utils/storage.js";
-
-import "./inspector-breadcrumbs.js";
-import "./inspector-link.js";
+import {IoButton} from "./button.js";
 
 function isValueOfPropertyOf(prop, object) {
   for (let key in object) if (object[key] === prop) return key;
@@ -248,3 +246,96 @@ IoInspector.Register();
 IoInspector.RegisterConfig = function(config) {
   this.prototype.__config.registerConfig(config);
 };
+
+export class IoInspectorBreadcrumbs extends IoElement {
+  static get style() {
+    return html`<style>
+      :host {
+        display: flex;
+        flex: 1 0;
+        flex-direction: row;
+        border: var(--io-theme-field-border);
+        border-radius: var(--io-theme-border-radius);
+        padding: var(--io-theme-padding);
+        color: var(--io-theme-field-color);
+        background: var(--io-theme-field-bg);
+      }
+      :host > io-inspector-link {
+        border: none;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        background: none;
+        padding: 0;
+        padding: var(--io-theme-padding);
+      }
+      :host > io-inspector-link:first-of-type {
+        color: var(--io-theme-color);
+        overflow: visible;
+        text-overflow: clip;
+        margin-left: 0.5em;
+      }
+      :host > io-inspector-link:last-of-type {
+        overflow: visible;
+        text-overflow: clip;
+        margin-right: 0.5em;
+      }
+      :host > io-inspector-link:not(:first-of-type):before {
+        content: '>';
+        margin: 0 0.5em;
+        opacity: 0.25;
+      }
+    </style>`;
+  }
+  static get properties() {
+    return {
+      crumbs: Array,
+    };
+  }
+  changed() {
+    this.template([this.crumbs.map(i => ['io-inspector-link', {value: i}])]);
+  }
+}
+
+IoInspectorBreadcrumbs.Register();
+
+export class IoInspectorLink extends IoButton {
+  static get style() {
+    return html`<style>
+      :host {
+        border: none;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        background: none;
+        padding: 0;
+        border: 1px solid transparent;
+        color: var(--io-theme-link-color);
+        padding: var(--io-theme-padding) !important;
+      }
+      :host:focus {
+        outline: none;
+        background: none;
+        text-decoration: underline;
+      }
+      :host:hover {
+        background: none;
+        text-decoration: underline;
+      }
+      :host[pressed] {
+        background: none;
+      }
+    </style>`;
+  }
+  changed() {
+    let name = this.value.constructor.name;
+    if (this.value.name) name += ' (' + this.value.name + ')';
+    else if (this.value.label) name += ' (' + this.value.label + ')';
+    else if (this.value.title) name += ' (' + this.value.title + ')';
+    else if (this.value.id) name += ' (' + this.value.id + ')';
+    this.title = name;
+    this.template([
+      ['span', name]
+    ]);
+  }
+}
+
+IoInspectorLink.Register();
