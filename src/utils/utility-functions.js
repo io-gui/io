caefc06cd9f604b1be776fbad23dd747be8a9193
Aki Rodic <aleksandar.xyz@gmail.com>
2019-06-23 20:51:43 +0200

    Added filterObject utility function.

--- src/utils/utility-functions.js
+++ src/utils/utility-functions.js
@@ -11,3 +11,15 @@ export function getObjectLabel(object) {
   else if (object.id) label += ' (' + object.id + ')';
   return label;
 }
+
+export function filterObject(object, predicate) {
+  if (predicate(object)) return object;
+  for (let key in object) {
+    if (predicate(object[key])) {
+        return object[key];
+    } else if (typeof object[key] === 'object') {
+      const prop = filterObject(object[key], predicate);
+      if (prop) return prop;
+    }
+  }
+}
