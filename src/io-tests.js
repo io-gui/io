f96143ab704cf71c1f07ab8a1134991bd94bf949
Aki Rodic <rodic@adobe.com>
2020-04-26 09:44:23 -0700

    Fixed tests style.

--- src/io-tests.js
+++ src/io-tests.js
@@ -93,6 +93,43 @@ function runTests() {
 }
 
 export class IoTests extends IoElement {
+  static get Style() {
+    return /* css */`
+      :host #mocha {
+        margin: 0;
+        position: relative;
+      }
+      :host #mocha-report {
+        margin: 2em 1em;
+      }
+      :host #mocha-stats {
+        position: absolute;
+        top: -2em;
+        right: 2em;
+        font-size: 12px;
+        margin: 0;
+      }
+      :host #mocha-stats li {
+        padding: 0;
+      }
+      :host #mocha-stats .progress {
+        display: none;
+      }
+      :host #mocha-stats .passes {
+        color: #0c0;
+      }
+      :host #mocha-stats .failures {
+        color: #f00;
+        font-weight: bold;
+      }
+      :host h2 {
+        padding-right: 2em;
+      }
+      :host .replay {
+        display: none !important;
+      }
+    `;
+  }
   connectedCallback() {
     super.connectedCallback();
     runTests();
