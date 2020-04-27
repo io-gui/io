99d87aba7f283f9f5f1046c95a4bd4ad8a7aad87
Aki Rodic <rodic@adobe.com>
2020-04-26 21:24:27 -0700

    Added Dictionary wip prototype.

---
+++ demos/wip/dictionary.js
@@ -0,0 +1,39 @@
+import {IoNode} from '../../build.io.js';
+
+class DictionaryItem extends IoNode {
+  static get Properties() {
+    return {
+      key: null,
+      value: null,
+      valueStore: Object,
+    };
+  }
+  constructor(props) {
+    super(props);
+    this.value = this.valueStore[this.key];
+  }
+  keyChanged() {
+    this.value = this.valueStore[this.key];
+  }
+}
+DictionaryItem.Register();
+
+class Dictionary extends IoNode {
+  static get Properties() {
+    return {
+      valueStore: Object,
+    };
+  }
+  registerValues(valueStore) {
+    Object.assign(this.valueStore, valueStore);
+  }
+  bind(binding) {
+    const item = new DictionaryItem({
+      key: binding,
+      valueStore: this.valueStore,
+    });
+    item.connect(this);
+    return item.bind('value');
+  }
+}
+Dictionary.Register();
