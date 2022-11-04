612d919d0899fae5f6b4f61c37b9720967f86984
Aki Rodic <aleksandar.xyz@gmail.com>
2022-11-04 16:12:41 +0100

    Color sliders WIP

--- src/elements/color/color-slider.ts
+++ src/elements/color/color-slider.ts
@@ -1,93 +1,91 @@
 import { RegisterIoElement } from '../../core/element.js';
 import { IoProperty } from '../../core/internals/property.js';
-import { IoSliderBase } from '../sliders/slider-base.js';
-import { IoColorMixin } from './color-base.js';
+import { IoColorBase } from './color-base.js';
+import { IoSlider } from '../sliders/slider.js';
 
-/*
- * Extends `IoColorMixin(IoSlider)`.
- *
- * Base class for color sliders for any color type.
- **/
+const COLORS = {
+  'r': [1, 0.25, 0.25, 1],
+  'g': [0.35, 0.75, 0.25, 1],
+  'b': [0.35, 0.35, 1, 1],
+  'a': [0.8, 0.8, 0.8, 1],
+};
+
 @RegisterIoElement
-export class IoColorSlider extends IoColorMixin(IoSliderBase) {
+export class IoColorSlider extends IoColorBase {
+  static get Style() {
+    return /* css */`
+      :host {
+        display: flex;
+        flex-direction: row;
+        flex: 0 1 17.2em;
+      }
+      :host[vertical] {
+        /* flex: 0 0 17.2em; */
+      }
+    `;
+  }
 
-  @IoProperty([0, 0, 0, 0])
-  declare value: [number, number, number, number];
+  @IoProperty('')
+  declare channel: string;
 
-  @IoProperty(0.001)
-  declare step: number;
+  @IoProperty({value: false, reflect: 'prop'})
+  declare vertical: boolean;
 
-  @IoProperty(0)
-  declare min: number;
-
-  @IoProperty(1)
-  declare max: number;
-
-  static get GlUtils() {
-    return /* glsl */`
-    vec4 paintColorSlider(vec2 position, vec3 color) {
-      // return paintSlider(position, color);
-      vec4 slotColor = vec4(.2, .2, .2, 1.);
-      vec4 fillColor = vec4(.8, .8, .8, 1.);
-      vec4 sliderColor = vec4(0.);
-      float slotWidth = ioStrokeWidth * 1.5;
-      float radius = ioFieldHeight / 4.;
-      float stroke = ioStrokeWidth;
-      float strokeShape = min(circle(position, radius + stroke), rectangle(position - vec2(0., 2500.), vec2(slotWidth + stroke, 5000.)));
-      sliderColor = mix(vec4(slotColor.rgb, 1.), sliderColor, strokeShape);
-      float fillShape = min(circle(position, radius), rectangle(position - vec2(0., 2500.), vec2(slotWidth, 5000.)));
-      sliderColor = mix(fillColor, sliderColor, fillShape);
-      float colorShape = min(circle(position, radius - stroke), rectangle(position - vec2(0., 2500.), vec2(slotWidth - stroke, 5000.)));
-      sliderColor = mix(vec4(color, 1.), sliderColor, colorShape);
-      return sliderColor;
-    }
-    vec4 paintColorSlider2D(vec2 position, vec3 color) {
-      vec4 sliderColor = vec4(0.);
-      float radius = ioLineHeight / 2.;
-      float stroke = ioStrokeWidth;
-      vec4 slotColor = vec4(.2, .2, .2, 1.);
-      vec4 fillColor = vec4(.8, .8, .8, 1.);
-      vec2 width = uVertical == 1 ? vec2(uSize.x, stroke * 2.) : vec2(stroke * 2., uSize.y);
-      float strokeShape = circle(position, radius + stroke);
-      sliderColor = mix(slotColor, sliderColor, strokeShape);
-      float fillShape = circle(position, radius);
-      sliderColor = mix(fillColor, sliderColor, fillShape);
-      float colorShape = circle(position, radius - stroke);
-      sliderColor = mix(vec4(color, 1.), sliderColor, colorShape);
-      return sliderColor;
-    }
-    \n\n`;
-  }
-  valueMutated() {
-    this.valueChanged();
-  }
-  deprecated_applyAria() {
-    // TODO
-  }
-  _onKeydown(event: KeyboardEvent) {
-    super._onKeydown(event);
-    this._notifyValueInput();
-  }
-  _setIncrease() {
-  }
-  _setDecrease() {
-  }
-  _setMin() {
-    this._inputValue(0, 0);
-  }
-  _setMax() {
-    this._inputValue(1, 1);
-  }
-  _onPointermoveThrottled(event: PointerEvent) {
-    super._onPointermoveThrottled(event);
-    this._notifyValueInput();
-  }
-  _notifyValueInput() {
-    this.dispatchEvent('object-mutated', {object: this.value}, false, window);
+  _onValueInput(event: CustomEvent) {
+    const c = this.channel as keyof typeof this.value;
+    const value = event.detail.value;
+    const oldValue = event.detail.oldValue;
+    this.value[c] = value;
+    const detail = {object: this.value, property: c, value: value, oldValue: oldValue};
+    this.dispatchEvent('object-mutated', detail, false, window);
     this.dispatchEvent('value-input', {property: 'value', value: this.value}, false);
   }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _inputValue(x: number, y?: number) {
-    // NOTE: implement in subclass
+
+  changed() {
+    const c = this.channel as keyof typeof this.value;
+
+    debug: {
+      if (['r', 'g', 'b', 'a', 'h', 's', 'v', 'l'].indexOf(c) === -1) {
+        console.warn('IoColorSlider: Incorrect channel value!', c);
+      }
+    }
+
+    if (['r', 'g', 'b', 'a'].indexOf(c) !== -1) {
+      this.template([
+        ['io-slider', {value: this.value[c], min: 0, max: 1, step: 0.001, vertical: this.vertical, color: COLORS[c], 'on-value-input': this._onValueInput}],
+      ]);
+    }
+
+    if (this.channel === 'h') {
+      this.template([
+        ['io-slider-h', {id: c, value: this.hsv[0], min: 0, max: 1, step: 0.1, vertical: this.vertical, color: [...this.hsv, 1], 'on-value-input': this._onValueInput}],
+      ]);
+    }
+
+  }
+}
+
+@RegisterIoElement
+export class IoSliderH extends IoSlider {
+  static get Frag() {
+    return /* glsl */`
+    varying vec2 vUv;
+
+    void main(void) {
+      // Dimensions
+      vec2 size = uVertical == 1 ? uSize.yx : uSize;
+      vec2 uv = uVertical == 1 ? vUv.yx : vUv;
+      vec2 position = size * (uv - vec2(0.0, 0.5));
+
+      // Colors
+      vec3 finalCol = hsv2rgb(vec3(uv.x, uColor[1], uColor[2]));
+      vec3 sliderCol = hsv2rgb(uColor.rgb);
+
+      // Slider
+      vec4 slider = paintSlider(position, size, sliderCol, sliderCol);
+      finalCol = mix(finalCol.rgb, slider.rgb, slider.a);
+
+      gl_FragColor = vec4(finalCol, 1.0);
+    }`;
   }
 }
--- src/elements/sliders/slider.ts
+++ src/elements/sliders/slider.ts
@@ -17,28 +17,73 @@ export class IoSlider extends IoSliderBase {
   @IoProperty(1)
   declare max: number;
 
+  static get GlUtils() {
+    return /* glsl */`
+      vec4 paintSlider(vec2 p, vec2 size, vec3 colorStart, vec3 colorEnd) {
+        vec4 finalCol = vec4(0.0);
+
+        float valueInRange = (uValue - uMin) / (uMax - uMin);
+        valueInRange = abs(pow(valueInRange, 1./uExponent));
+
+        vec2 start = vec2(0.0, size.y * 0.0);
+        vec2 end = vec2(size.x * min(2.0, max(-1.0, (valueInRange))), 0.0);
+        start.x = min(start.x, end.x);
+
+        vec2 pStart = translate(p, start);
+        vec2 pEnd = translate(p, end);
+        vec2 pCenter = (pStart + pEnd) / 2.0;
+        float slotHalfWidth = abs(pStart.x - pEnd.x) / 2.0;
+
+        float slotThickness = ioFieldHeight * 0.125;
+
+        float strokeShape = min(
+          rectangle(pCenter, vec2(slotHalfWidth, slotThickness + ioStrokeWidth + ioStrokeWidth)),
+          circle(pEnd, slotThickness + ioStrokeWidth + ioStrokeWidth)
+        );
+
+        float fillShape   = min(
+          rectangle(pCenter, vec2(slotHalfWidth, slotThickness + ioStrokeWidth)),
+          circle(pEnd, slotThickness + ioStrokeWidth)
+        );
+        float colorShape  = min(
+          rectangle(pCenter, vec2(slotHalfWidth, slotThickness)),
+          circle(pEnd, slotThickness)
+        );
+
+        float grad = (p.x - start.x) / (end.x - start.x);
+        vec3 slotGradient = mix(colorStart, colorEnd, saturate(grad));
+
+        finalCol = mix(ioColor, finalCol, strokeShape);
+        finalCol = mix(vec4(ioBackgroundColor.rgb, 1.0), finalCol, fillShape);
+        finalCol = mix(vec4(slotGradient, 1.0), finalCol, colorShape);
+
+        return finalCol;
+      }
+    `;
+  }
+
   static get Frag() {
     return /* glsl */`
     varying vec2 vUv;
 
     void main(void) {
+      // Dimensions
+      vec2 size = uVertical == 1 ? uSize.yx : uSize;
+      vec2 uv = uVertical == 1 ? vUv.yx : vUv;
+      vec2 position = size * (uv - vec2(0.0, 0.5));
+
       // Colors
       vec3 finalCol = ioBackgroundColorField.rgb;
       vec3 gridCol = mix(ioColor.rgb, ioBackgroundColorField.rgb, 0.75);
       vec3 sliderColStart = ioColorFocus.rgb;
       vec3 sliderColEnd = ioColorLink.rgb;
 
-      // Dimensions
-      vec2 size = uVertical == 1 ? uSize.yx : uSize;
-      vec2 uv = uVertical == 1 ? vUv.yx : vUv;
-      vec2 position = size * (uv - vec2(0.0, 0.5));
-
       // Sizes
       float gridThickness = ioStrokeWidth;
       float gridSize = size.x / abs((uMax - uMin) / uStep);
       float gridOffset = mod(uMin, uStep) / (uMax - uMin) * size.x;
-      float slotWidth = ioFieldHeight * 0.125;
 
+      // Grid
       vec2 expPosition = size * vec2(pow(uv.x, uExponent), uv.y - 0.5);
       vec2 gridPosition = translate(expPosition, gridOffset, 0.0);
       float gridShape = grid2d(gridPosition, vec2(gridSize, size.y + gridThickness * 2.0), gridThickness);
@@ -47,13 +92,8 @@ export class IoSlider extends IoSliderBase {
         finalCol.rgb = mix(gridCol.rgb, finalCol.rgb, gridShape);
       }
 
-      float valueInRange = (uValue - uMin) / (uMax - uMin);
-      valueInRange = abs(pow(valueInRange, 1./uExponent));
-
-      vec2 sliderStart = vec2(0.0, size.y * 0.0);
-      vec2 sliderEnd = vec2(size.x * min(2.0, max(-1.0, (valueInRange))), 0.0);
-      sliderStart.x = min(sliderStart.x, sliderEnd.x);
-      vec4 slider = paintSlider(position, sliderStart, sliderEnd, slotWidth, slotWidth, sliderColStart, sliderColEnd);
+      // Slider
+      vec4 slider = paintSlider(position, size, sliderColStart, sliderColEnd);
       finalCol = mix(finalCol.rgb, slider.rgb, slider.a);
 
       gl_FragColor = vec4(finalCol, 1.0);
