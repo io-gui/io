333e1f2e36572cc97b6a1a138b2c8d80093cab51
Aki Rodic <aleksandar.xyz@gmail.com>
2022-11-19 09:03:50 +0100

    Refactoring path WIP.

--- src/models/path.ts
+++ src/models/path.ts
@@ -1,20 +1,31 @@
 import {IoNode, RegisterIoNode} from '../core/node.js';
+import { Property } from '../core/internals/property.js';
+import { Binding } from '../core/internals/binding.js';
 
 @RegisterIoNode
 export class Path extends IoNode {
-  static get Properties() {
-    return {
-      value: Array,
-      string: String,
-      root: null,
-      leaf: null,
-      delimiter: ':',
-    };
-  }
-  constructor(...args: any[]) {
-    super(...args);
+
+  @Property({type: Array})
+  // @Property([]) // TODO: consider prohibiting this pattern!
+  declare value: any[];
+
+  @Property({value: null})
+  declare root: any;
+
+  @Property({value: null})
+  declare leaf: any;
+
+  @Property('')
+  declare string: string;
+
+  @Property(':')
+  declare delimiter: string;
+
+  init() {
+    // TODO: make sure minimal number of instances is used
     this.valueChanged();
   }
+
   valueChanged() {
     // TODO: redesign. Investigate why inputValue causes infinite loop.
     this._properties.get('value')!.value = new Proxy(this._properties.get('value')!.value, {
@@ -34,23 +45,40 @@ export class Path extends IoNode {
     this.dispatchQueue();
   }
   update() {
-    let string = '';
-    for (let i = 0; i < this.value.length; i++) {
-      debug: {
-        if (this.value[i] && typeof this.value[i] === 'string' && this.value[i].search(this.delimiter) !== -1) {
-          console.warn(`IoGUI Path: Value ${this.value[i]} with special string "${this.delimiter}" cannot be used in path!`);
-          break;
-        }
-      }
-      string += this.value[i];
-      if (i !== this.value.length - 1) string += this.delimiter;
-    }
     this.setProperties({
-      'string': string,
+      'string': this._valueToString(this.value),
       'leaf': this.value[this.value.length - 1],
       'root': this.value[0],
     });
   }
+
+  // valueChanged() {
+  //   this.setProperties({
+  //     'string': this._valueToString(this.value),
+  //     'leaf': this.value[this.value.length - 1] || '',
+  //     'root': this.value[0] || '',
+  //   });
+  // }
+
+  _valueToString(value: string[]) {
+    let string = '';
+    for (let i = 0; i < value.length; i++) {
+      const val = value[i];
+      debug: {
+        if (val && typeof val === 'string' && val.search(this.delimiter) !== -1) {
+          console.warn(`IoGUI Path: Value ${val} with special string "${this.delimiter}" cannot be used in path!`);
+          break;
+        }
+      }
+      // if (typeof val === 'string' || typeof val === 'number') {
+        string += val;
+      // }
+      if (i !== value.length - 1 && String(value[value.length - 1]) !== '') string += this.delimiter;
+    }
+    // console.log(string);
+    return string;
+  }
+
   stringChanged() {
     const array = this.string ? [...this.string.split(this.delimiter)] : [];
     for (let i = 0; i < array.length; i++) {
@@ -58,15 +86,34 @@ export class Path extends IoNode {
     }
     this.value.length = array.length;
   }
+
   rootChanged() {
-    if (this.value[0] !== this.root) {
-      this.value = [this.root];
+    if (this.root !== undefined) {
+      if (this.value[0] !== this.root) {
+        this.value.length = 0;
+        this.value.push(this.root);
+        this.setProperties({
+          string: this.root !== undefined ? this.root : '',
+          leaf: this.root,
+        });
+        this.dispatchEvent('object-mutated', {object: this.value}, false, window);
+      }
+    } else {
+      this.value.length = 0;
+      this.setProperties({
+        string: '',
+        leaf: undefined,
+      });
+      this.dispatchEvent('object-mutated', {object: this.value}, false, window);
     }
   }
-  leafChanged() {
-    const i = Math.max(0, this.value.length - 1);
-    if (this.value[i] !== this.leaf) {
-      this.value[i] = this.leaf;
+
+  bind(prop: string): Binding {
+    debug: {
+      if (prop === 'leaf') {
+        console.warn('Path: Binding to "leaf" property not recommended by design!');
+      }
     }
+    return super.bind(prop);
   }
 }
