622103e1a64bf6399c6a7f0392fcd018d527ea56
Aki Rodic <aleksandar.xyz@gmail.com>
2022-11-22 23:39:24 +0100

    Fixed demo style bug.

--- demos/todomvc/todo-info.js
+++ demos/todomvc/todo-info.js
@@ -2,7 +2,7 @@ import { IoMdView, RegisterIoElement } from '../../build/iogui.js';
 
 export class TodoInfo extends IoMdView {
   changed() {
-    this.parseMarkdown(`
+    this._parseMarkdown(`
 Double-click to edit a todo
 
 Created by [Aki Rodic](https://akirodic.com)
