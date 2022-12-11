fd5bea0c8b7e48b536b7750b366406080254a3da
Aki Rodic <aleksandar.xyz@gmail.com>
2022-12-12 00:35:19 +0100

    Fixed io-color-rgba number channel mapping.

--- src/elements/color/io-color-rgba.ts
+++ src/elements/color/io-color-rgba.ts
@@ -26,23 +26,25 @@ export class IoColorRgba extends IoColorBase {
     `;
   }
 
-  _onValueInput(event: CustomEvent) {
-    const item = event.composedPath()[0];
-    const c = (item as any).id as keyof typeof this.value;
-    const value = event.detail.value;
+  _onNumberValueInput(event: CustomEvent) {
+    const item = event.composedPath()[0] as HTMLElement;
+    const id = item.id as keyof typeof this.value;
+    const newValue = event.detail.value;
     const oldValue = event.detail.oldValue;
-    this.value[c] = value;
-    const detail = {object: this.value, property: c, value: value, oldValue: oldValue};
+    const value = this.value as any;
+    value[id] = newValue;
+
+    const detail = {object: this.value, property: id, value: value, oldValue: oldValue};
     this.dispatchEvent('object-mutated', detail, false, window);
-    this.dispatchEvent('value-input', {property: 'value', value: this.value}, false);
   }
 
   changed() {
     this.template([
-      ['io-number', {$: 'r', value: this.value.r, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onValueInput}],
-      ['io-number', {$: 'g', value: this.value.g, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onValueInput}],
-      ['io-number', {$: 'b', value: this.value.b, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onValueInput}],
-      this.value.a !== undefined ? ['io-number', {$: 'a', value: this.value.a, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onValueInput}] : null,
+      // Consider removing global id collisions.
+      ['io-number', {$: 'r', id: 'r', value: this.value.r, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onNumberValueInput}],
+      ['io-number', {$: 'g', id: 'g', value: this.value.g, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onNumberValueInput}],
+      ['io-number', {$: 'b', id: 'b', value: this.value.b, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onNumberValueInput}],
+      this.value.a !== undefined ? ['io-number', {$: 'a', value: this.value.a, min: 0, max: 1, step: 0.0001, ladder: true, 'on-value-input': this._onNumberValueInput}] : null,
       ['io-color-picker', {$: 'swatch', value: this.value}],
     ]);
   }
